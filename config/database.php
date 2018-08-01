cd700a09bc1e4c8000d6e99765a68a8976e0a483
Jan Daryl Galbo <jandarylgalbo@gmail.com>
2018-07-31 22:40:49 -0400

    Update the configs.

--- config/database.php
+++ config/database.php
@@ -39,7 +39,7 @@ return [
 
         'mysql' => [
             'driver'      => 'mysql',
-            'host'        => env('DB_HOST', 'https://db4free.net'),
+            'host'        => env('DB_HOST', 'db4free.net'),
             'port'        => env('DB_PORT', '3306'),
             'database'    => env('DB_DATABASE', 'sciemms'),
             'username'    => env('DB_USERNAME', 'sciemms'),
