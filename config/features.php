511648ca16b0d3596002e291a68aa26c6424720c
Jan Daryl Galbo <jandarylgalbo@gmail.com>
2018-08-21 10:26:34 -0400

    Change the form-submission feature default to true

--- config/features.php
+++ config/features.php
@@ -8,7 +8,7 @@ return [
      */
 
     'multi-language'     =>    env('MULTI_LANGUAGES_ENABLED',   false),
-    'form-submissions'   =>    env('FORM_SUBMISSION_ENABLED',   false),
+    'form-submissions'   =>    env('FORM_SUBMISSION_ENABLED',   true),
     'form-settings'      =>    env('FORM_SETTING_ENABLED',      false),
     'metas'              =>    env('METAS_ENABLED',             false),
     'redirection'        =>    env('REDIRECION_ENABLED',        false),
