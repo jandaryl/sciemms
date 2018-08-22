867d7b9b79c0c7fc20a3f4d9aea4505097bc37d6
Jan Daryl Galbo <jandarylgalbo@gmail.com>
2018-08-21 20:19:07 -0400

    Add cachable trait in form-submission, permission and social login models

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use GeneaLabs\LaravelModelCaching\Traits\Cachable;
 use Illuminate\Database\Eloquent\Model;
 
 class Permission extends Model
 {
+    use Cachable;
+
     protected $fillable = [
         'name',
     ];
