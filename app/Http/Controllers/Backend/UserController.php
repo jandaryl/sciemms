840305b0da73418eae70a845d9d4a1bfd8053bb8
Jan Daryl Galbo <jandarylgalbo@gmail.com>
2018-09-23 04:24:15 -0400

    Refactor the User Controller

--- app/Http/Controllers/Backend/UserController.php
+++ app/Http/Controllers/Backend/UserController.php
@@ -34,16 +34,158 @@ class UserController extends BackendController
         $this->roles = $roles;
     }
 
+
     /**
-     * Show the application dashboard.
+     * Search the data from the request.
      *
      * @param Request $request
-     *
-     * @throws \Exception
-     *
      * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator|\Symfony\Component\HttpFoundation\BinaryFileResponse
      */
     public function search(Request $request)
+    {
+        return $this->requestData($request);
+    }
+
+
+    /**
+     * Show the user.
+     *
+     * @param User $user
+     * @return User
+     */
+    public function show(User $user)
+    {
+        $this->cannotEditUser($user);
+
+        return $user;
+    }
+
+    /**
+     * Get all the roles.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public function getRoles()
+    {
+        return $this->roles->getAllowedRoles();
+    }
+
+
+    /**
+     * Store the user request to database.
+     *
+     * @param StoreUserRequest $request
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function store(StoreUserRequest $request)
+    {
+        $this->canCreateUsers();
+
+        $this->users->store($request->input());
+
+        return $this->redirectResponse($request, __('alerts.backend.users.created'));
+    }
+
+
+    /**
+     * Update the user data from database.
+     *
+     * @param User $user
+     * @param UpdateUserRequest $request
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function update(User $user, UpdateUserRequest $request)
+    {
+        $this->canEditUsers();
+
+        $this->users->update($user, $request->input());
+
+        return $this->redirectResponse($request, __('alerts.backend.users.updated'));
+    }
+
+
+    /**
+     * Delete the user from database.
+     *
+     * @param User $user
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function destroy(User $user, Request $request)
+    {
+        $this->canDeleteUsers();
+
+        $this->users->destroy($user);
+
+        return $this->redirectResponse($request, __('alerts.backend.users.deleted'));
+    }
+
+
+    /**
+     * Impersonate the users.
+     *
+     * @param User $user
+     * @return mixed
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function impersonate(User $user)
+    {
+        $this->canImpersonateUsers();
+
+        return $this->users->impersonate($user);
+    }
+
+
+    /**
+     * Perform a batch action in users.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function batchAction(Request $request)
+    {
+        $action = $request->get('action');
+        $ids = $request->get('ids');
+
+        switch ($action) {
+            case 'destroy':
+                return $this->deleteUsers($request, $ids);
+                break;
+            case 'enable':
+                return $this->enableUsers($request, $ids);
+                break;
+            case 'disable':
+                return $this->disableUsers($request, $ids);
+                break;
+        }
+
+        return $this->redirectResponse($request, __('alerts.backend.actions.invalid'), 'error');
+    }
+
+    /**
+     * Toggle the active user.
+     *
+     * @param User $user
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function activeToggle(User $user)
+    {
+        $this->canEditUsers();
+
+        $user->update(['active' => ! $user->active]);
+    }
+
+    /**
+     * Search the data.
+     *
+     * @param Request $request
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator|\Symfony\Component\HttpFoundation\BinaryFileResponse
+     */
+    public function requestData(Request $request)
     {
         $requestSearchQuery = new RequestSearchQuery($request, $this->users->query(), [
             'name',
@@ -85,123 +227,107 @@ class UserController extends BackendController
     }
 
     /**
-     * @param User $user
+     * Check the user if it cannot edit the user.
      *
-     * @return User
+     * @param User $user
      */
-    public function show(User $user)
+    public function cannotEditUser(User $user): void
     {
-        if (! $user->can_edit) {
+        if ( ! $user->can_edit) {
             // Only Super admin can access himself
             abort(403);
         }
-
-        return $user;
-    }
-
-    public function getRoles()
-    {
-        return $this->roles->getAllowedRoles();
     }
 
     /**
-     * @param StoreUserRequest $request
+     * Check the user if it can create the users.
      *
-     * @return mixed
+     * @throws \Illuminate\Auth\Access\AuthorizationException
      */
-    public function store(StoreUserRequest $request)
+    public function canCreateUsers(): void
     {
         $this->authorize('create users');
-
-        $this->users->store($request->input());
-
-        return $this->redirectResponse($request, __('alerts.backend.users.created'));
     }
 
     /**
-     * @param User              $user
-     * @param UpdateUserRequest $request
+     * Check the user if it can edit the users.
      *
-     * @throws \Illuminate\Database\Eloquent\MassAssignmentException
-     *
-     * @return mixed
+     * @throws \Illuminate\Auth\Access\AuthorizationException
      */
-    public function update(User $user, UpdateUserRequest $request)
+    public function canEditUsers(): void
     {
         $this->authorize('edit users');
-
-        $this->users->update($user, $request->input());
-
-        return $this->redirectResponse($request, __('alerts.backend.users.updated'));
     }
 
     /**
-     * @param User    $user
-     * @param Request $request
+     * Check the user if it can delete the users.
      *
-     * @return mixed
+     * @throws \Illuminate\Auth\Access\AuthorizationException
      */
-    public function destroy(User $user, Request $request)
+    public function canDeleteUsers(): void
     {
         $this->authorize('delete users');
-
-        $this->users->destroy($user);
-
-        return $this->redirectResponse($request, __('alerts.backend.users.deleted'));
     }
 
     /**
-     * @param User $user
+     * Check the user if it can impersonate the users.
      *
-     * @return mixed
+     * @throws \Illuminate\Auth\Access\AuthorizationException
      */
-    public function impersonate(User $user)
+    public function canImpersonateUsers(): void
     {
         $this->authorize('impersonate users');
+    }
 
-        return $this->users->impersonate($user);
+
+    /**
+     * Check the user if it can  delete the users.
+     *
+     * @param Request $request
+     * @param $ids
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function deleteUsers(Request $request, $ids)
+    {
+        $this->canDeleteUsers();
+
+        $this->users->batchDestroy($ids);
+
+        return $this->redirectResponse($request, __('alerts.backend.users.bulk_destroyed'));
     }
 
     /**
-     * @param \Illuminate\Http\Request $request
+     * Check the user if it can enable the users.
      *
+     * @param Request $request
+     * @param $ids
      * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
      */
-    public function batchAction(Request $request)
+    public function enableUsers(Request $request, $ids)
     {
-        $action = $request->get('action');
-        $ids = $request->get('ids');
+        $this->canEditUsers();
 
-        switch ($action) {
-            case 'destroy':
-                $this->authorize('delete users');
+        $this->users->batchEnable($ids);
 
-                $this->users->batchDestroy($ids);
-
-                return $this->redirectResponse($request, __('alerts.backend.users.bulk_destroyed'));
-                break;
-            case 'enable':
-                $this->authorize('edit users');
-
-                $this->users->batchEnable($ids);
-
-                return $this->redirectResponse($request, __('alerts.backend.users.bulk_enabled'));
-                break;
-            case 'disable':
-                $this->authorize('edit users');
-
-                $this->users->batchDisable($ids);
-
-                return $this->redirectResponse($request, __('alerts.backend.users.bulk_disabled'));
-                break;
-        }
-
-        return $this->redirectResponse($request, __('alerts.backend.actions.invalid'), 'error');
+        return $this->redirectResponse($request, __('alerts.backend.users.bulk_enabled'));
     }
 
-    public function activeToggle(User $user)
+    /**
+     * Check the user if it can disable the users.
+     *
+     * @param Request $request
+     * @param $ids
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function disableUsers(Request $request, $ids)
     {
-        $this->authorize('edit users');
-        $user->update(['active' => ! $user->active]);
+        $this->canEditUsers();
+
+        $this->users->batchDisable($ids);
+
+        return $this->redirectResponse($request, __('alerts.backend.users.bulk_disabled'));
     }
 }
