aea6f69e2b91762049e21c8d0bce194fe699edf5
Jan Daryl Galbo <jandarylgalbo@gmail.com>
2018-09-25 06:42:08 -0400

    Test the submit registration form

--- tests/BrowserKitTestCase.php
+++ tests/BrowserKitTestCase.php
@@ -2,6 +2,7 @@
 
 namespace Tests;
 
+use Arcanedev\NoCaptcha\Facades\NoCaptcha;
 use Laravel\BrowserKitTesting\TestCase as BaseTestCase;
 
 abstract class BrowserKitTestCase extends BaseTestCase
@@ -13,5 +14,15 @@ abstract class BrowserKitTestCase extends BaseTestCase
     protected function user()
     {
         return factory(\App\Models\User::class)->create();
+    }
+
+    protected function ignoreCaptcha($name = 'g-recaptcha-response')
+    {
+        NoCaptcha::shouldReceive('display')
+            ->andReturn('<input type="checkbox" value="yes" name="' . $name . '">');
+        NoCaptcha::shouldReceive('script')
+            ->andReturn('<script src="captcha.js"></script>');
+        NoCaptcha::shouldReceive('verify')
+            ->andReturn(true);
     }
 }
