1b46e8d551fa73ce9a8f9e751be5dea26fb414f4
Jan Daryl Galbo <jandarylgalbo@gmail.com>
2018-09-29 11:14:13 -0400

    Add documentation in store index.js, actions.js, mutations.js and router index.js

--- resources/assets/js/backend/store/index.js
+++ resources/assets/js/backend/store/index.js
@@ -1,12 +1,30 @@
+/**
+ * Use the Vue and Vuex
+ */
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+/**
+ * Use the actions and mutations functions.
+ */
 import { createActions } from './actions'
 import mutations from './mutations'
 
+/**
+ * It will register the Vuex in the Vue instance.
+ */
 Vue.use(Vuex)
 
+/**
+ * Function that connect the whole logic of Vuex.
+ * It will use actions and mutations JS files.
+ * Set the state and wait for AJAX data.
+ * Update the counters properties.
+ */
 export function createStore (route) {
+  /**
+   * Call the createActions function.
+   */
   const actions = createActions(route)
 
   return new Vuex.Store({
--- resources/assets/js/backend/store/mutations.js
+++ resources/assets/js/backend/store/mutations.js
@@ -1,3 +1,7 @@
+/**
+ * The mutation is all about storing of latest commit from actions.
+ * It will get the data from the actions.js.
+ */
 export default {
   SET_COUNTER: (state, { type, counter }) => {
     state.counters[type] = counter
