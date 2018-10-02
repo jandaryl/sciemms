eec19a4768acded7c08735d7dde4b108f4888170
Jan Daryl Galbo <jandarylgalbo@gmail.com>
2018-10-01 22:02:30 -0400

    Add the helpers and snippets for vue testing

--- tests/Unit/Backend/Example.test.js
+++ tests/Unit/Backend/Example.test.js
@@ -2,9 +2,34 @@ import { mount } from '@vue/test-utils'
 import Component from '@/Counter.vue'
 
 describe('Component', () => {
-  test('is a Vue instance', () => {
+  it('is a Vue instance', () => {
     const wrapper = mount(Component)
-    expect(wrapper.isVueInstance()).toBeTruthy()
-    expect(wrapper.element).toMatchSnapshot()
+    assertTrue(wrapper.isVueInstance())
+    assertSnapMatched(wrapper.element)
   })
 })
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+///                                    Vue Test Helper wrapper                                       ///
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+let assertTrue = data => {
+  expect(data).toBeTruthy()
+}
+
+let assertSnapMatched = element => {
+  expect(element).toMatchSnapshot()
+}
