d39ba1b2fb01af9cc7ba6a8043e11a89adce0287
Jan Daryl Galbo <jandarylgalbo@gmail.com>
2018-10-18 01:40:55 -0400

    Add a php docs in Redirection model file

--- app/Models/Redirection.php
+++ app/Models/Redirection.php
@@ -6,9 +6,15 @@ use Illuminate\Support\Facades\Gate;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 
+/**
+ * Class Redirection
+ * @package App\Models
+ */
 class Redirection extends Model
 {
-
+    /**
+     * @var array
+     */
     protected $fillable = [
         'source',
         'active',
@@ -16,25 +22,47 @@ class Redirection extends Model
         'type',
     ];
 
+    /**
+     * @var array
+     */
     protected $casts = [
         'active' => 'boolean',
     ];
 
+    /**
+     * @var array
+     */
     protected $appends = [
         'can_edit',
         'can_delete'
     ];
 
+    /**
+     * Append the can_edit in this model.
+     *
+     * @return bool
+     */
     public function getCanEditAttribute()
     {
         return true;
     }
 
+    /**
+     * Append the can_delete in this model.
+     *
+     * @return bool
+     */
     public function getCanDeleteAttribute()
     {
         return Gate::check('delete redirections');
     }
 
+    /**
+     * Filter the actives of this model.
+     *
+     * @param Builder $query
+     * @return Builder
+     */
     public function scopeActives(Builder $query)
     {
         return $query->where('active', '=', true);
